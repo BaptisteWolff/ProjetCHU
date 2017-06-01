b13ecf7e0521e76d3e36738d1a542989ab29fbf2
BaptisteWolff <baptist21@live.fr>
2017-06-01 11:48:48 +0200

    Launcher.bat : lance toutes les applications

---
+++ Launcher.bat
@@ -0,0 +1,13 @@
+@echo off
+
+start Generateur\Generateur\bin\Debug\Generateur.exe
+
+start PilotageBras\PilotageBras\bin\Debug\PilotageBras.exe
+
+start Signal\Signal\bin\Debug\Signal.exe
+
+start WCFServerPico\bin\Debug\WCFServer.exe
+
+start WCFClient\bin\Debug\TestWCFClient.exe
+
+exit
